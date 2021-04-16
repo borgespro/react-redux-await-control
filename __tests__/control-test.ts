70b769de6756b604d9ca5966735c22bb11f7f648
Thiago Borges <thiago@borges.pro>
2021-04-16 12:44:33 -0300

    Create possibility to add key in async action

--- __tests__/control-test.ts
+++ __tests__/control-test.ts
@@ -14,33 +14,45 @@ export function createTestStore(): Store {
 describe('Testing integration with redux', () => {
   const fetchTodosActionName = 'FETCH_TODOS';
   const fetchTodosAction = createAsyncAction(fetchTodosActionName);
+  const removeTodoActionName = 'REMOVE_TODO';
+  const removeTodoAction = createAsyncAction(removeTodoActionName);
 
   describe('async actions valid flux', () => {
     let store;
+    const meta = { awaitId: 1 };
 
     beforeEach(() => {
       store = createTestStore();
       store.dispatch(fetchTodosAction.start());
+      store.dispatch(removeTodoAction.start(true, meta));
     });
 
     it('verify is starting with START status', () => {
       expect(store.getState().control[fetchTodosActionName]).toBeDefined();
       expect(store.getState().control[fetchTodosActionName]).toEqual('START');
+      expect(store.getState().control[`${removeTodoActionName}_${meta.awaitId}`]).toBeDefined();
+      expect(store.getState().control[`${removeTodoActionName}_${meta.awaitId}`]).toEqual('START');
     });
 
     it('create user case with fetch result success.', () => {
       store.dispatch(fetchTodosAction.success());
       expect(store.getState().control[fetchTodosActionName]).toEqual('SUCCESS');
+      store.dispatch(removeTodoAction.success(true, meta));
+      expect(store.getState().control[`${removeTodoActionName}_${meta.awaitId}`]).toEqual('SUCCESS');
     });
 
     it('create user case with fetch result cancel.', () => {
       store.dispatch(fetchTodosAction.cancel());
       expect(store.getState().control[fetchTodosActionName]).toEqual('CANCEL');
+      store.dispatch(removeTodoAction.cancel(meta));
+      expect(store.getState().control[`${removeTodoActionName}_${meta.awaitId}`]).toEqual('CANCEL');
     });
 
     it('create user case with fetch result failure.', () => {
-      store.dispatch(fetchTodosAction.failure());
+      store.dispatch(fetchTodosAction.failure(new Error()));
       expect(store.getState().control[fetchTodosActionName]).toEqual('FAILURE');
+      store.dispatch(removeTodoAction.failure(new Error(), meta));
+      expect(store.getState().control[`${removeTodoActionName}_${meta.awaitId}`]).toEqual('FAILURE');
     });
   });
 
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,21 +1,41 @@
-import { Action } from 'redux-actions';
+import { ActionMeta } from 'redux-actions';
 
 import { AsyncActionReducer, AsyncActionState } from './types';
 import { CANCEL, FAILURE, START, SUCCESS } from './constants';
 
-export default function reducer(state: AsyncActionReducer = {}, action: Action<any>): AsyncActionReducer {
+const formatRequestName = (requestName: string, { payload, meta }: ActionMeta<any, any>) => {
+  const actionPayloadMeta: any = {};
+
+  if (typeof payload === 'object') {
+    Object.assign(actionPayloadMeta, payload);
+  }
+
+  if (typeof meta === 'object') {
+    Object.assign(actionPayloadMeta, meta);
+  }
+
+  if (actionPayloadMeta.awaitId) {
+    return `${requestName}_${actionPayloadMeta.awaitId.toString().toUpperCase()}`;
+  }
+
+  return requestName;
+};
+
+export default function reducer(state: AsyncActionReducer = {}, action: ActionMeta<any, any>): AsyncActionReducer {
   const matches = /(.*)\/(.*)_(START|SUCCESS|FAILURE|CANCEL)/.exec(action.type);
 
   if (!matches) return state;
 
   const [, , requestName, requestState] = matches;
 
-  if (state[requestName] === START && [SUCCESS, FAILURE, CANCEL].includes(requestState)) {
-    return { ...state, [requestName]: requestState as AsyncActionState };
+  const formattedRequestName = formatRequestName(requestName, action);
+
+  if (state[formattedRequestName] === START && [SUCCESS, FAILURE, CANCEL].includes(requestState)) {
+    return { ...state, [formattedRequestName]: requestState as AsyncActionState };
   }
 
   if (requestState === START) {
-    return { ...state, [requestName]: START };
+    return { ...state, [formattedRequestName]: START };
   }
 
   return state;
