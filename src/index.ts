dae6d06919f516b1be39a96dcfab556c09cd844d
Thiago Borges <thiago@borges.pro>
2021-04-18 22:14:44 -0300

    Implementing useAwaitControl only react hook part

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import createAsyncAction from './createAsyncAction';
 import reducer from './reducer';
 import AwaitControl from './AwaitControl';
+import useAwaitControl from './useAwaitControl';
 
-export { createAsyncAction, reducer };
+export { createAsyncAction, reducer, useAwaitControl };
 export default AwaitControl;
