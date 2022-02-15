42c7458d3701751ef5024869577c7feff5e31b41
Thiago Borges <thiago@borges.pro>
2022-02-15 11:57:46 -0300

    Code improvements

---
+++ .changeset/cyan-socks-obey.md
@@ -0,0 +1,5 @@
+---
+'react-redux-await-control': patch
+---
+
+Taking "get" function to utils file
--- src/get.ts
+++
@@ -1,8 +0,0 @@
-export default function get(obj: any, path?: string) {
-  if (!path) return obj;
-  const keys = path.split('.');
-  return keys.reduce((value, key) => {
-    if (!value) return undefined;
-    return value[key];
-  }, obj);
-}
